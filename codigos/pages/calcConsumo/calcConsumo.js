f9ca1ed7a926f19bbeed2f0933d11a184b12da6f
babi-barbato <barbara.oliveira@sptech.school>
2023-04-07 20:44:46 -0300

    Style(all) Todas as telas estão padronizadas

--- codigos/pages/calcConsumo/calcConsumo.js
+++ codigos/pages/calcConsumo/calcConsumo.js
@@ -1,11 +1,19 @@
 function calculo() {
-  let horasDia = Number(iptHorasDia.value);
-  let diasMes = Number(iptDiasMes.value);
-  let consumoAparelho = Number(iptConsumoAparelho.value);
-  let precEnergia = Number(iptPreçoEnergia.value);
-  let horasMes = horasDia * diasMes;
-  let consumoDia = consumoAparelho / 30;
-  let calculo = Math.round((horasMes * consumoDia * precEnergia)*100)/100;
+  //declaração de variaveis
+  var horasDia = Number(iptHorasDia.value);
+  var diasMes = Number(iptDiasMes.value);
+  var consumoAparelho = Number(iptConsumoAparelho.value);
+  var precEnergia = Number(iptPreçoEnergia.value);
+
+  //calculando a quantidade de horas o mes
+  var horasMes = horasDia * diasMes;
+
+  //calculando o consumo por dia
+  var consumoDia = consumoAparelho / 30;
+
+  //calculando quanto sera gastado
+  var calculo = Math.round((horasMes * consumoDia * precEnergia)*100)/100;
+
   if (calculo > 0) {
     txtValorAoVivo.innerHTML = `R$ ${calculo}`;
   } else {
