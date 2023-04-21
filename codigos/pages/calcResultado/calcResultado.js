027ae71bfb6154b0d99ba58480a3f70d6fcbbd8e
giomenezes <giovanna.rodrigues@sptech.school>
2023-04-21 13:09:06 -0300

    refactor(calculadoras): Incrementei o toFixed as calculadoras e inseri um link ao produto

--- codigos/pages/calcResultado/calcResultado.js
+++
@@ -1,9 +0,0 @@
-var btu=localStorage.getItem("resultado");
-var consumo=localStorage.getItem("calculo");
-
-var total = (Number (btu) + Number (consumo));
-
-// setar 'var btu=localStorage.getItem("resultado")' nas outras calculadoras?
-valorTotal.innerHTML = `${total}`
-
-// Adicionar o script scr das outras calculadoras no html? Mas como soma dai MEUDEUS SOCORRO AAAAAAAAAAAAAAAAAAAAAA
