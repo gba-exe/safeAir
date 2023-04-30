9b3db06277ebc55488e9da0666f4e0ca04f5a1b5
babi-barbato <barbara.oliveira@sptech.school>
2023-04-30 16:37:19 -0300

    Feat(dashboard, menuDashboard) Mudança no layout do menu e novo menu lateral

--- codigos/pages/dashboard/dashboard.js
+++ codigos/pages/dashboard/dashboard.js
@@ -1,12 +1,11 @@
-const temperatura = document.getElementById('temperaturaProblema');
-
+const temperatura = document.getElementById('temperaturaSalas');
   new Chart(temperatura, {
     type: 'line',
     data: {
-      labels: ['01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '00:00'],
+      labels: ['Diretoria','Descanso','Refeitório','Biblioteca','T.I.','Reunião','Conferência','Hall','Secretaria'],
       datasets: [{
-        label: 'Temperatura - Sala 2',
-        data: [18, 19, 23, 25, 22, 26,18, 20, 23, 25, 25, 26,18, 15, 14, 13, 15, 20, 19, 22, 23, 21, 20, 22],
+        label: 'Temperatura das Salas - °C',
+        data: [18, 19, 23, 25, 22, 26,18, 20, 23],
         backgroundColor: [
             '#f04600'
         ],
@@ -21,15 +20,25 @@ const temperatura = document.getElementById('temperaturaProblema');
         y: {
           beginAtZero: true
         }
+      },
+      plugins: {
+        legend: {
+          labels: {
+              font: {
+                  size: 18
+              }
+          }
+        }
       }
+      
     }
   });
 
-const umidade = document.getElementById('umidadeProblema');
+const umidade = document.getElementById('umidadeSalas');
   new Chart(umidade, {
     type: 'line',
     data: {
-      labels: ['01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '00:00'],
+      labels: ['Diretoria','Descanso','Refeitório','Biblioteca','T.I.','Reunião','Conferência','Hall','Secretaria'],
       datasets: [{
         label: 'Umidade - Sala 2',
         data: [40, 49, 43, 45, 50, 56, 48, 40, 40, 45, 35, 36, 38, 40, 44, 53, 55, 57, 60, 62, 50, 45, 40, 42],
@@ -46,6 +55,15 @@ const umidade = document.getElementById('umidadeProblema');
       scales: {
         y: {
           beginAtZero: true
+        }
+      },
+      plugins: {
+        legend: {
+          labels: {
+              font: {
+                  size: 18
+              }
+          }
         }
       }
     }
