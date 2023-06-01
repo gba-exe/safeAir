be6e9c98384fb89c24274e27d80686a825142ce4
gba-exe <gabriel.baraujo@sptech.school>
2023-06-01 12:33:11 -0300

    cleanup(): aerofolio para meu palio.

--- server/web-data-viz/site/src/models/usuarioModel.js
+++ server/web-data-viz/site/src/models/usuarioModel.js
@@ -31,14 +31,8 @@ function cadastrar(nomeFunc, emailFunc, senhaFunc) {
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
-<<<<<<< HEAD:a/server/web-data-viz/site/src/models/usuarioModel.js
-
-function cadastrarEmpresa(nome_Empresa, cnpj) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarEmpresa():", nome_Empresa, cnpj);
-=======
 function cadastrarEmpresa(nome_Empresa, cnpj, estado, cidade, bairro, rua, numero, complemento, cep, nomeUsuario, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarEmpresa():", nome_Empresa, cnpj, estado, cidade, bairro, rua, numero, complemento, cep);
->>>>>>> production:server/web-data-viz/site/src/models/usuarioModel.js
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
@@ -121,11 +115,26 @@ function alterarSenha(novaSenhaFunc, idFunc) {
     return database.executar(instrucao);
 }
 
+
+function cadastrarSala(cnpjSala, tamanhoSala, nomeSala) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarSala():", cnpjSala, tamanhoSala, nomeSala);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var inserirSala = `
+        INSERT INTO sala (idSala, nomeSala, tamanhoSala, fkEndereco, fkEmpresa) VALUES (NULL, '${nomeSala}', '${tamanhoSala}', '${cnpjSala}', '${cnpjSala}');
+    `;
+    console.log("Executando a instrução SQL: \n" + inserirSala);
+    return database.executar(inserirSala);
+}
+
 module.exports = {
     entrar,
     cadastrar,
     listar,
     cadastrarEmpresa,
     registrarFunc,
-    alterarSenha
+    alterarSenha,
+    cadastrarSala
+
 };
